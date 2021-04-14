ec1c89447d2ed40e4bba191f678c5cc88be766b4
Hartono Tanaja <Hartonotanaja@live.com>
2021-04-14 20:28:50 +0700

    footer animation

--- assets/main.js
+++ assets/main.js
@@ -17,9 +17,28 @@ function updateSize() {
 }
 var activities = document.getElementById("confirmbuttoncontainer");
 var target = document.getElementById("footerBox");
+var sosmed = document.querySelectorAll(".comlogo");
+
+var closebutton = document.getElementById("closebutton");
+
+
+
+
 activities.addEventListener("click", () => {
-   
-    gsap.to(target,{duration:0.5, height:"85%", y:"-70%", ease:"power4"});
+    var tl=gsap.timeline();
+    tl.to(target,{duration:0.5, height:"80vh", y:"-60vh", opacity:100, ease:"power4"});
+    tl.to(sosmed,{duration:1, y:"65vh", ease:"yoyo"},"-=0.4");
+    tl.to(closebutton,{duration:0.1, visibility:"visible"});
 });
+
+closebutton.addEventListener("click", ()=>{
+    var tl=gsap.timeline();
+    tl.to(closebutton,{duration:0.1,visibility:"hidden"});
+    tl.to(target,{duration:0.5, height:"20vh", y:"0vh", opacity:"70%",ease:"power4"});
+    tl.to(sosmed,{duration:0.5, y:"0vh",ease:"power4"},"-=0.5");
+
+})
+
+
 
 
