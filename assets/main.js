35c9bf705c6db5e6ddad5478ce0f89330adbec0c
Hartono Tanaja <Hartonotanaja@live.com>
2021-04-14 21:34:15 +0700

    add detail item

--- assets/main.js
+++ assets/main.js
@@ -18,7 +18,7 @@ function updateSize() {
 var activities = document.getElementById("confirmbuttoncontainer");
 var target = document.getElementById("footerBox");
 var sosmed = document.querySelectorAll(".comlogo");
-
+var detail = document.getElementById("detail");
 var closebutton = document.getElementById("closebutton");
 
 
@@ -28,12 +28,14 @@ activities.addEventListener("click", () => {
     var tl=gsap.timeline();
     tl.to(target,{duration:0.5, height:"80vh", y:"-60vh", opacity:100, ease:"power4"});
     tl.to(sosmed,{duration:1, y:"65vh", ease:"yoyo"},"-=0.4");
+    tl.to(detail,{duration:0.5,opacity:100, ease:"power4"});
     tl.to(closebutton,{duration:0.1, visibility:"visible"});
 });
 
 closebutton.addEventListener("click", ()=>{
     var tl=gsap.timeline();
     tl.to(closebutton,{duration:0.1,visibility:"hidden"});
+    tl.to(detail,{duration:0.5,opacity:0});
     tl.to(target,{duration:0.5, height:"20vh", y:"0vh", opacity:"70%",ease:"power4"});
     tl.to(sosmed,{duration:0.5, y:"0vh",ease:"power4"},"-=0.5");
 
