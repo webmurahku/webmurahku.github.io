f9d6302c7a9c9ba276044db214678d15154a362b
Hartono Tanaja <Hartonotanaja@live.com>
2021-04-14 18:17:21 +0700

    fix footer

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,5 @@
+console.log("This javascript is working");
+
 //get the correct window size 
 updateSize();
 
@@ -11,4 +13,13 @@ function updateSize() {
     let vh = window.innerHeight * 0.01;
     //set the vh value in the --vh custom prop to the root of dom
     document.documentElement.style.setProperty('--vh', '${vh}px');
+    console.log(vh);
 }
+var activities = document.getElementById("confirmbuttoncontainer");
+var target = document.getElementById("footerBox");
+activities.addEventListener("click", () => {
+    console.log("clicked");
+    gsap.to(target,{duration:0.5, height:"85%", y:"-70%", ease:"power4"});
+});
+
+
