778cf2be8580843777b8aa1504703f6d5949efb6
webmurahku <webmurahku@outlook.com>
2021-04-17 18:06:40 +0700

    sosmed button

--- assets/main.js
+++ assets/main.js
@@ -5,6 +5,9 @@ var sosmed = document.querySelectorAll(".logoCom");
 var detail = document.getElementById("detailBox");
 var confirmButton = document.getElementById("confirmButton");
 var closeButton = document.getElementById("closeButton");
+var whatsappButton = document.getElementById("whatsappButton");
+var facebookButton = document.getElementById("facebookButton");
+var instagramButton = document.getElementById("instagramButton");
 //var label = document.getElementById("text");
 
 var blueBubble = document.getElementById("blueBubble");
@@ -13,13 +16,24 @@ var tl = gsap.timeline();
 tl.to(blueBubble, { duration: 5, y: "5vh", repeat: -1, ease: "power1", yoyo: true, delay: 1 });
 tl.to(purpleBubble, { duration: 5, x: "3vw", repeat: -1, yoyo: true }, "-=4.5");
 //var closebutton = document.getElementById("closeButton");
+var isPortrait = (window.innerHeight/window.innerWidth) > 1;
 
 confirmButton.addEventListener("click", () => {
+    
     var tl = gsap.timeline();
     detail.style.visibility="visible";
-    tl.to(target, { duration: 0.5, height: "80vh", y: "0", opacity: 100, ease: "power4" });
-    tl.to(sosmed,{duration:1, y:"67vh", ease:"bounce"},"-=0.4");
-    tl.to(detail,{duration:0.5, opacity:100, ease:"power1.in"});
+    
+    if(isPortrait){
+        tl.to(target, { duration: 0.5, height: "80vh", y: "0", opacity: 100, ease: "power4" });
+        tl.to(sosmed,{duration:1, y:"67vh", ease:"bounce"},"-=0.4");
+        tl.to(detail,{duration:0.5, opacity:100, ease:"power1.in"});
+    }
+    else{
+        tl.to(target, { duration: 0.5, height: "70vh", y: "0", opacity: 100, ease: "power4" });
+        tl.to(sosmed,{duration:1, y:"60vh", ease:"bounce"},"-=0.4");
+        tl.to(detail,{duration:0.5, opacity:100, ease:"power1.in"});
+    }
+    
     closeButton.style.visibility = "visible";
 
 });
@@ -27,13 +41,39 @@ confirmButton.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
     closeButton.style.visibility = "hidden";
     var tl = gsap.timeline();
-    tl.to(detail,{duration:0.5, opacity:0});
-    tl.to(target, { duration: 0.5, height: "20vh", y: "0vh", ease: "power4" });
-    tl.to(sosmed,{duration:0.5, y:"0vh",ease:"power4"},"-=0.5");
+
+    if(isPortrait){
+        tl.to(detail,{duration:0.5, opacity:0});
+        tl.to(target, { duration: 0.5, height: "20vh", y: "0vh", ease: "power4" });
+        tl.to(sosmed,{duration:0.5, y:"0vh",ease:"power4"},"-=0.5");
+    }
+    else{
+        tl.to(detail,{duration:0.5, opacity:0});
+        tl.to(target, { duration: 0.5, height: "10vh", y: "0vh", ease: "power4" });
+        tl.to(sosmed,{duration:0.5, y:"0vh",ease:"power4"},"-=0.5");
+    }
+    
     detail.style.opacity = 0;
     detail.style.visibility = "hidden";
     
 });
+
+whatsappButton.addEventListener("click",()=>{
+    alert("whatsapp");
+});
+
+facebookButton.addEventListener("click",()=>{
+    alert("facebook");
+});
+
+instagramButton.addEventListener("click",()=>{
+    alert("instagram");
+})
+
+
+
+
+
 
 
 
