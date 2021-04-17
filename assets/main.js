8a3607a7e8857c5be3044d327c3d1d18d0c8dc55
webmurahku <webmurahku@outlook.com>
2021-04-17 16:07:15 +0700

    fix layout and animation

--- assets/main.js
+++ assets/main.js
@@ -1,33 +1,39 @@
 console.log("This javascript is working");
 
 var target = document.getElementById("footerBox");
-var sosmed = document.querySelectorAll(".comlogo");
-var detail = document.getElementById("detail");
-var confimrbutton = document.getElementById("confirmButton");
-
+var sosmed = document.querySelectorAll(".logoCom");
+var detail = document.getElementById("detailBox");
+var confirmButton = document.getElementById("confirmButton");
+var closeButton = document.getElementById("closeButton");
+//var label = document.getElementById("text");
 
 var blueBubble = document.getElementById("blueBubble");
 var purpleBubble = document.getElementById("purpleBubble");
 var tl = gsap.timeline();
-tl.to(blueBubble,{duration:5, y:50, repeat:-1, yoyo:true});
-tl.to(purpleBubble,{duration:5, x:20, repeat:-1, yoyo:true});
+tl.to(blueBubble, { duration: 5, y: "5vh", repeat: -1, ease: "power1", yoyo: true, delay: 1 });
+tl.to(purpleBubble, { duration: 5, x: "3vw", repeat: -1, yoyo: true }, "-=4.5");
 //var closebutton = document.getElementById("closeButton");
 
-//activities.addEventListener("click", () => {
-//    var tl=gsap.timeline();
-//    tl.to(target,{duration:0.5, height:"80vh", y:"-60vh", opacity:100, ease:"power4"});
-//    tl.to(sosmed,{duration:1, y:"65vh", ease:"yoyo"},"-=0.4");
-//    tl.to(detail,{duration:0.5,opacity:100, ease:"power4"});
-//    tl.to(closebutton,{duration:0.1, visibility:"visible"});
-//});
+confirmButton.addEventListener("click", () => {
+    var tl = gsap.timeline();
+    detail.style.visibility="visible";
+    tl.to(target, { duration: 0.5, height: "80vh", y: "0", opacity: 100, ease: "power4" });
+    tl.to(sosmed,{duration:1, y:"67vh", ease:"bounce"},"-=0.4");
+    tl.to(detail,{duration:0.5, opacity:100, ease:"power1.in"});
+    closeButton.style.visibility = "visible";
 
-//closebutton.addEventListener("click", ()=>{
-//    var tl=gsap.timeline();
-//    tl.to(closebutton,{duration:0.1,visibility:"hidden"});
-//    tl.to(detail,{duration:0.5,opacity:0});
-//    tl.to(target,{duration:0.5, height:"20vh", y:"0vh", opacity:"70%",ease:"power4"});
-//    tl.to(sosmed,{duration:0.5, y:"0vh",ease:"power4"},"-=0.5");
-//});
+});
+
+closeButton.addEventListener("click", () => {
+    closeButton.style.visibility = "hidden";
+    var tl = gsap.timeline();
+    tl.to(detail,{duration:0.5, opacity:0});
+    tl.to(target, { duration: 0.5, height: "20vh", y: "0vh", ease: "power4" });
+    tl.to(sosmed,{duration:0.5, y:"0vh",ease:"power4"},"-=0.5");
+    detail.style.opacity = 0;
+    detail.style.visibility = "hidden";
+    
+});
 
 
 
