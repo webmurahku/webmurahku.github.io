2205f62f00832cd310adf41bb5b868d0ec0bed4a
webmurahku <webmurahku@outlook.com>
2021-05-07 16:52:26 +0700

    animation next

--- JS/main.js
+++ JS/main.js
@@ -4,23 +4,37 @@ const el = document.createElement("H2");
 
 heroContainer.append(el);
 
-animeText(el, "Jaman NOW\njualan kudu ONLINE\nkamu SUDAH?", 2);
+animeText(heroContainer, el, "Jaman NOW\njualan kudu ONLINE\nkamu SUDAH?", 2);
 
 
 
 ///////// FUNCTIONS TO DETACH
 
-async function animeText(el, words, timer) {
+async function animeText(container, el, words, timer) {
 
   word = words.split("\n");
-  console.log(word.length);
+  const lengths = word.map(a=>a.length);
+  const longest = lengths.indexOf(Math.max(...lengths));
+  
+  el.textContent = word[longest];
+  var textRatio = el.offsetWidth/el.offsetHeight;
+  textHeight = container.offsetWidth * 0.7 /textRatio;
+  el.style.fontSize = textHeight+"px";
 
-  while(true){await anime(el, word, timer)};
- 
+  
+  await anime(el, word, timer)
+    
+  
   async function anime(el, word, timer) {
     for (i = 0; i < word.length; i++) {
       el.textContent = word[i];
-      await gsap.from(el, { duration: `${timer}`, fontSize: "10px", ease: "power1.out" });
+      await gsap.from(el, { duration: `${timer}`, fontSize: "1px", ease: "power1.out" });
     }
   }
 }
+
+const nextButton = document.getElementById("nextSectionButton");
+
+nextButton.addEventListener("click",()=>{
+    gsap.to("body",{duration:1,y:"-100vh",ease:"power4.out"});
+});
